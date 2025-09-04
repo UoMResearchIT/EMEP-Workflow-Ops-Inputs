4d3d2dae4a3ed506b0f9e77a858e4be96aec4fe5
Douglas Lowe <10961945+douglowe@users.noreply.github.com>
2025-09-04 17:15:15 +0100

    post proc scripts

---
+++ PostProc/Data_Extraction/batch_extract_template.sh
@@ -0,0 +1,26 @@
+#!/bin/bash --login
+#SBATCH -t 0-6
+#SBATCH -p serial
+#SBATCH -J DATA-%%DOM%%-%%JOBID%%
+
+source ~/bin/conda_activate.sh 
+conda activate wrf-python
+
+CWD=%%POSTPROCDIR%%
+
+STARTYEAR=%%YRST%%
+STARTMONTH=%%MONST%%
+STARTDAY=%%DAYST%%
+
+ENDYEAR=%%YREND%%
+ENDMONTH=%%MONEND%%
+ENDDAY=%%DAYEND%%
+
+WRFDIR=%%WRFDIR%%
+EMEPDIR=%%EMEPDIR%%
+OUTPUTDIR=%%OUTDIR%%
+
+
+python data_extraction.py --startyear $STARTYEAR --startmonth $STARTMONTH --startday $STARTDAY \
+						--endyear $ENDYEAR --endmonth $ENDMONTH --endday $ENDDAY \
+						--wrfdir $WRFDIR --emepdir $EMEPDIR --outdir $OUTPUTDIR
---
+++ PostProc/Data_Extraction/data_extraction.py
@@ -0,0 +1,111 @@
+import xarray as xr
+from wrf import getvar, to_np, latlon_coords
+import netCDF4 as nc
+import argparse
+
+
+def get_latlon_shape(ds):
+   pressureSample = getvar(ds, "pressure", timeidx=0)
+   lat, lon = latlon_coords(pressureSample)
+   south_north = pressureSample.sizes['south_north']
+   west_east = pressureSample.sizes['west_east']
+   bottom_top = pressureSample.sizes['bottom_top']
+   return(lat, lon, south_north, west_east, bottom_top)
+
+def load_3d_wrf_data(ds, t, varName, outArray):
+    varData = getvar(ds, varName, timeidx=t)
+    outArray[t, :, :] = to_np(varData)
+
+def load_4d_wrf_data(ds, t, varName, outArray):
+    varData = getvar(ds, varName, timeidx=t)
+    outArray[t, :, :, :] = to_np(varData)
+
+def load_4d_emep_data(ds, t, varName, outArray):
+    varData = ds[varName][t, :, :, :]
+    outArray[t, :, :, :] = to_np(varData)
+
+#def load_4d_emep_PM25_data(ds, t, outArray):
+    
+    
+def main(wrfDir, emepDir, outputDir, wrfFile, emepFile, outFile):
+   
+   wrfDS = nc.Dataset(wrfDir+wrfFile)
+   emepDS = nc.Dataset(emepDir+emepFile)
+      
+   ntimes = wrfDS.dimensions["Time"]
+   
+   lat, lon, south_north, west_east, bottom_top = get_latlon_shape(wrfDS)
+
+   with nc.Dataset(outputDir+outFile, "w", format="NETCDF4") as out:
+        out.createDimension("Time", None)
+        out.createDimension("south_north", south_north)
+        out.createDimension("west_east", west_east)
+        out.createDimension("bottom_top", bottom_top)
+        
+        out.createVariable("XLAT", "f4", ("south_north", "west_east"))[:] = to_np(lat)
+        out.createVariable("XLONG", "f4", ("south_north", "west_east"))[:] = to_np(lon)
+
+        u10_var = out.createVariable("U10", "f4", ("Time", "south_north", "west_east"))
+        v10_var = out.createVariable("V10", "f4", ("Time", "south_north", "west_east"))
+        t2_var  = out.createVariable("T2",  "f4", ("Time", "south_north", "west_east"))
+        ua_var  = out.createVariable("U",   "f4", ("Time", "bottom_top", "south_north", "west_east"))
+        va_var  = out.createVariable("V",   "f4", ("Time", "bottom_top", "south_north", "west_east"))
+
+        o3_var  = out.createVariable("O3", "f4", ("Time", "bottom_top", "south_north", "west_east")) 
+
+        for t in range(ntimes.size):
+            load_3d_wrf_data(wrfDS, t, "U10", u10_var)
+            load_3d_wrf_data(wrfDS, t, "V10", v10_var)
+            load_3d_wrf_data(wrfDS, t, "T2", t2_var)
+
+            load_4d_wrf_data(wrfDS, t, "ua", ua_var)
+            load_4d_wrf_data(wrfDS, t, "va", va_var)
+            
+            load_4d_emep_data(emepDS, t, "O3", o3_var)
+
+
+def parse_cli_arguments():
+    parser = argparse.ArgumentParser(description="WRF/EMEP Data Extraction and Collation")
+    
+    parser.add_argument("-y", "--year", required=True, help="Data Year")
+    parser.add_argument("-m", "--month", required=True, help="Data Month")
+    parser.add_argument("-d", "--day", required=True, help="Data Day")
+    
+    parser.add_argument("-w", "--wrfdir", required=True, help="WRF data directory")
+    parser.add_argument("-e", "--emepdir", required=True, help="EMEP data directory")
+    parser.add_argument("-o", "--outdir", required=True, help="Output data directory")
+
+    parser.add_argument("-n", "--wrfdomain", required=False, default="d01", help="WRF domain")
+
+    return vars(parser.parse_args())
+    
+    
+
+if __name__ == "__main__":
+    print('get arguments')
+    
+    args = parse_cli_arguments()
+
+    year = args["year"]
+    month = args["month"]
+    day = args["day"]
+    
+    wrfDir = args["wrfdir"]
+    emepDir = args["emepdir"]
+    outputDir = args["outdir"]
+
+    wrfdom = args["wrfdomain"]
+
+    
+    print('start data extraction')
+    
+    
+    wrfFile = f"wrfout_{wrfdom}_{year}-{month}-{day}_00:00:00"
+    emepFile = f"EMEP_OUT_{year}{month}{day}.nc"
+    
+    outFile = f"WRF_EMEP_{year}{month}{day}.nc"
+    
+    main(wrfDir, emepDir, outputDir, wrfFile, emepFile, outFile)
+    
+    print('finished data extraction')
+    
